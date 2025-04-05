8114b625d29f2f79bb65043affa37af292d3ee98
mwq <1640277939@qq.com>
2025-04-05 21:45:35 +0800

    测试

---
+++ frontend/vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  productionSourceMap: false,
+  devServer: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
+  }
+} 
